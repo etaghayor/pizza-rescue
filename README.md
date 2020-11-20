2c8f66ea45076c4ac9e6d53f6a16ca52d13a1cce
MaelChemeque <maelchemeque@gmail.com>
2020-11-20 21:17:01 +0100

    edit README.md

--- README.md
+++ README.md
@@ -15,3 +15,34 @@
 - Game environment : where we choose the level, the player's progression. We can also make a demonstration and present him the rules.
 - Board : Case[][], be careful to play in the cases 
 - Add a bot who could play instead of a human ?
+
+## Running the software 
+
+ It can be run with several options. In order to pass program arguments, you have to do :
+ 
+ ```
+ javac PetRescue.java
+ ```
+ 
+ And then if : 
+ 
+ ```
+ java PetRescue ...
+ ```
+ Or ... :
+ 
+ ``` 
+ java PetRescue ... 
+ ```
+ Or even to let's a bot play :
+  
+ ```
+ java PetRescue ...
+ ```
+ You can show the help with the following command : 
+ 
+ ```
+ java PetRescue -h
+ ```
+ 
+ 
--- src/PetRescue.java
+++ src/PetRescue.java
@@ -1,12 +1,20 @@
 import view.GUI;
 import view.UI;
 
+/*import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+*/
+
 public class PetRescue {
+	
     public static void main(String[] args) {
-    	/** The only thing we have to do is to create whether the
-    	 *  GUI or UI constructor and call his function
-    	 */
-    	GUI graphicalInterface = new GUI();
-    	UI textInterface = new UI();    	
+    	
+    	//GUI graphicalInterface = new GUI();
+    	//UI textInterface = new UI();    	
     }
+    
 }
