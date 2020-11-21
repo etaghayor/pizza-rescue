0ff3a6a87f42846689117f2f7f030d4341e49ec6
MaelChemeque <maelchemeque@gmail.com>
2020-11-21 14:24:56 +0100

    better PetRescue.java

--- src/PetRescue.java
+++ src/PetRescue.java
@@ -17,36 +17,49 @@ public class PetRescue {
 		options.addOption("t", "text", false, "Run the program in console");
 		options.addOption("g", "graphical", false, "Run the program in a window");
 		options.addOption("l", "level", true, "Choose the level you want to play");
-		options.addOption("b", "bot", false, "Let's a bot play the game");
+		options.addOption("b", "bot", false, "Let's a bot play the game on graphical interface");
 		options.addOption("h", "help", false, "Shows this help");
 		
 		try {
 			CommandLine commandLine = parser.parse(options, args);
-			if (commandLine.hasOption("l")) {
-				if (validLevel(commandLine.getOptionValue("l"))) {
-					if (commandLine.hasOption("t")) {
-						System.out.println("t");
-						/* Call the text user interface 
-						 * and select the level
-						 */
-					} else if (commandLine.hasOption("g")) {
-						System.out.println("g");
-						/* Call the graphical user interface 
-						 * and select the level
-						 */
-					} else if (commandLine.hasOption("b")) {
-						//TODO : make the bot gameplay method
-					} else if (commandLine.hasOption("h")) {
+			if(args.length != 0) {
+				if (commandLine.hasOption("l")) {
+					if (validLevel(commandLine.getOptionValue("l"))) {
+						if (commandLine.hasOption("t")) {
+							System.out.println("text method");
+							/* Call the text user interface 
+							 * and select the level
+							 */
+						} else if (commandLine.hasOption("g")) {
+							System.out.println("graphical method");
+							/* Call the graphical user interface 
+							 * and select the level
+							 */
+						} else if (commandLine.hasOption("b")) {
+							System.out.println("bot method");
+							//TODO : make the bot gameplay method
+						} else if (commandLine.hasOption("h")) {
+							displayHelpAndExit();
+						} else {
+							System.out.println("text method by default");
+							/* Call the text user interface 
+							 * and select the level
+							 * by default
+							 */
+						}
+					} else {
 						displayHelpAndExit();
 					}
+				} else if (commandLine.hasOption("h")){
+					displayHelpAndExit();
 				} else {
-					System.err.println("1");
+					System.err.println("You must use the -l argument with an int option and then -b, -g, or -t");
+					displayHelpAndExit();
 				}
-			}	
-		 else {
-			System.err.println("Wrong command");
-			displayHelpAndExit();
-		 }
+			} else {
+				System.out.println("Text method by default");
+				//TODO : call the first level
+			}
 		} catch (ParseException e) {
 			System.err.println("Wrong command");
 			displayHelpAndExit();
@@ -55,32 +68,23 @@ public class PetRescue {
 	}
 	static boolean validLevel(String level) {
 		try {
-			switch(Integer.parseInt(level)) {
-			case 0 :
-				System.out.println(0);
-				break;
-			case 1 : 
-				System.out.println(1);
-				break;
-			case 2 : 
-				System.out.println(2);
-				break;
-			case 3 : 
-				System.out.println(3);
-				break;
+			int levelInt = Integer.parseInt(level);
+			if (levelInt > 0 && levelInt < 10) {
+				return true;
+			} else {
+				throw new IllegalArgumentException();
 			}
 		} catch (Exception e) {
-			System.err.println("You must declare an int between ...");	
-			displayHelpAndExit();
-		}
-		return true;
-		//TODO : check if the level is playable
+			System.err.println("You must declare the -l argument with an int behind between ...");	
+			return false;
+		}		
 	}
 	static void displayHelpAndExit() {
 		String header = "Make sure you add at least the -l argument (obligatory) with an int option";
 		String footer ="";
 		HelpFormatter formatter = new HelpFormatter();
 		formatter.printHelp("CommandLineParameters", header, options, footer, true);
+		System.exit(1);
 	}
 	
     public static void main(String[] args) {
